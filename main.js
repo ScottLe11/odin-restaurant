0b38453f1466e5c7ddfafddfc70fb404b8039213
Scott Le <scottle220044@gmail.com>
2026-01-17 17:20:16 -0800

    Deployment commit

---
+++ main.js
@@ -0,0 +1,167 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/contact.js"
+/*!************************!*\
+  !*** ./src/contact.js ***!
+  \************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   loadContactPage: () => (/* binding */ loadContactPage)\n/* harmony export */ });\nfunction loadContactPage(){\n    const content = document.getElementById(\"content\");\n    const heading = document.createElement(\"h1\");\n\n    heading.textContent = \"This is the Contact page\";\n    content.appendChild(heading);\n}\n\n//# sourceURL=webpack://odin-restaurant/./src/contact.js?\n}");
+
+/***/ },
+
+/***/ "./src/index.js"
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _initial_page_load_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./initial-page-load.js */ \"./src/initial-page-load.js\");\n/* harmony import */ var _menu_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./menu.js */ \"./src/menu.js\");\n/* harmony import */ var _contact_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./contact.js */ \"./src/contact.js\");\n\n\n\n\nconsole.log(\"Works!!\")\n\n\nconst content = document.getElementById(\"content\");\n\nfunction clearContent(){\n    content.innerHTML = \"\";\n}\n\nconst homeBtn = document.querySelector(\"#homeBtn\");\nconst menuBtn = document.querySelector(\"#menuBtn\");\nconst contactBtn = document.querySelector(\"#contactBtn\");\n\n(0,_initial_page_load_js__WEBPACK_IMPORTED_MODULE_0__.loadHomePage)();\n\nhomeBtn.addEventListener(\"click\", () => {\n    clearContent();\n    (0,_initial_page_load_js__WEBPACK_IMPORTED_MODULE_0__.loadHomePage)(); \n});\n\nmenuBtn.addEventListener(\"click\", () => {\n    clearContent();\n    console.log(\"Menu button clicked!\");\n    (0,_menu_js__WEBPACK_IMPORTED_MODULE_1__.loadMenuPage)(); \n});\n\ncontactBtn.addEventListener(\"click\", () => {\n    clearContent();\n    (0,_contact_js__WEBPACK_IMPORTED_MODULE_2__.loadContactPage)(); \n});\n\n//loadHomePage();\n\n\n\n//# sourceURL=webpack://odin-restaurant/./src/index.js?\n}");
+
+/***/ },
+
+/***/ "./src/initial-page-load.js"
+/*!**********************************!*\
+  !*** ./src/initial-page-load.js ***!
+  \**********************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   loadHomePage: () => (/* binding */ loadHomePage)\n/* harmony export */ });\n/* harmony import */ var _pho_recipe_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./pho-recipe.jpg */ \"./src/pho-recipe.jpg\");\n\nfunction loadHomePage() { \n    const content = document.getElementById(\"content\");\n    const heading = document.createElement(\"h1\");\n    const pho_img = document.createElement(\"img\");\n    const description = document.createElement(\"h3\");\n\n    heading.textContent = \"Hello this is my new page for my restaurant!!!\";\n    pho_img.src = _pho_recipe_jpg__WEBPACK_IMPORTED_MODULE_0__;\n    description.textContent = \"We hope you enjoy\";\n\n    content.appendChild(heading);\n    content.appendChild(description);\n    content.appendChild(pho_img);\n}\n\n//# sourceURL=webpack://odin-restaurant/./src/initial-page-load.js?\n}");
+
+/***/ },
+
+/***/ "./src/menu.js"
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   loadMenuPage: () => (/* binding */ loadMenuPage)\n/* harmony export */ });\nfunction loadMenuPage(){\n    const content = document.getElementById(\"content\");\n    const heading = document.createElement(\"h1\");\n\n    heading.textContent = \"This is the Menu page\";\n    content.appendChild(heading);\n}\n\n//# sourceURL=webpack://odin-restaurant/./src/menu.js?\n}");
+
+/***/ },
+
+/***/ "./src/pho-recipe.jpg"
+/*!****************************!*\
+  !*** ./src/pho-recipe.jpg ***!
+  \****************************/
+(module, __unused_webpack_exports, __webpack_require__) {
+
+eval("{module.exports = __webpack_require__.p + \"c1e69e1d714e78a248a4.jpg\";\n\n//# sourceURL=webpack://odin-restaurant/./src/pho-recipe.jpg?\n}");
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Check if module exists (development only)
+/******/ 		if (__webpack_modules__[moduleId] === undefined) {
+/******/ 			var e = new Error("Cannot find module '" + moduleId + "'");
+/******/ 			e.code = 'MODULE_NOT_FOUND';
+/******/ 			throw e;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT')
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && (!scriptUrl || !/^http(s?):/.test(scriptUrl))) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/^blob:/, "").replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
