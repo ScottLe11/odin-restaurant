d60935f1ba71fcbb325ccf2419db6828699422ea
Scott Le <scottle220044@gmail.com>
2026-01-17 17:37:01 -0800

    Testing deploy automation

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,6 @@ export function loadMenuPage(){
     const content = document.getElementById("content");
     const heading = document.createElement("h1");
 
-    heading.textContent = "This is the Menu page";
+    heading.textContent = "This is the Menu page!!!";
     content.appendChild(heading);
 }
